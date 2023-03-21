69ab67244b159a32e1302f97001742f6cb2231ff
sotelo27 <lmsotelo@fi.uba.ar>
2023-03-21 17:38:01 -0300

    ejercicios clase 5

--- Clase 5/GUIA DE EJERCICIO 5/ArchivoEntrega.js
+++ Clase 5/GUIA DE EJERCICIO 5/ArchivoEntrega.js
@@ -187,17 +187,154 @@ arrayCadenas();
 
 //EJ12
 
+/*
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form>
+        Nombre del cliente:
+        <input type = 'text' placeholder="ingrese su nombre aqui" size="20" id = "nombre">
+        <br>
+        Seleccione el material de la caja: 
+        <select class="" name="Material" id = "material">
+            <option value = "Madera" >Madera</option>
+            <option value = "Acero" >Acero</option>
+            <option value = "Plastico" >Plastico</option>
+        </select><br>
+        <div >
+            Seleccione el Tamanio: <br>
+            <input type="radio" name="tamanio" value="Diminuto" id="diminuto" /> Diminuto
+            <input type="radio" name="tamanio" value="Mediano" id="mediano" /> Mediano
+            <input type = "radio" name="tamanio" value="Grande" id = "grande"/> Grande
+        </div >
+        Comentarios:<br>
+        <textarea name = "Comentarios" rows = "5" cols="25" id = "comentarios"></textarea>
+        <p></p>
+        <button onclick="mostrarValoresSeleccionados()">Enviar</button> <button></button>
+    </form>
+    <script>
+        function mostrarValoresSeleccionados(){
+            let valor = document.getElementById("diminuto").value
+            if (document.getElementById("mediano").checked){
+                valor = document.getElementById("mediano").value;
+            }else{
+                valor = document.getElementById("grande").value;
+            }
+            let valores = document.getElementById("nombre").value + " ha pedido una caja de " + document.getElementById("material").value + " con unas dimensiones " + valor +"." + document.getElementById("comentarios").value + ".";
+            alert(valores);
+        }
+    </script>
+</body>
+</html>
+*/
 
 //-----------------------------------------------------//
 
 
 //EJ13
 
+/*
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Seleccione a continuacion uno de los siguientes numeros, tiene un total de 3 intentos para acertar el numero.
+    <select class = "" name="valores" id="numeros">
+        <option value = 0  onclick="evaluarCorrecta(this.value)">0</option>
+        <option value = 1 onclick="evaluarCorrecta(this.value)">1</option>
+        <option value = 2 onclick="evaluarCorrecta(this.value)">2</option>
+        <option value = 3 onclick="evaluarCorrecta(this.value)" >3</option>
+        <option value = 4 onclick="evaluarCorrecta(this.value)">4</option>
+        <option value = 5 onclick="evaluarCorrecta(this.value)">5</option>
+    </select>
+    <script>
+        let valor = Math.floor(Math.random()*5);
+        valor = valor.toString();
+        let intentos = 0;
+        console.log(valor);
+        console.log(intentos);
+        function evaluarCorrecta(valorElegido){
+            if (valorElegido === valor){
+                alert("ACERTASTE!!!");
+                valor = Math.floor(Math.random()*5);
+                valor = valor.toString();
+                intentos = 0;
+            }else if (intentos !==3){
+                intentos +=1;
+            }else{
+                alert("PERDISTE!!!!");
+                valor = Math.floor(Math.random()*5);
+                valor = valor.toString();
+                intentos = 0;
+            }
+            console.log(valor);
+            console.log(intentos);
+        }
+    </script>
+</body>
+</html>
+*/
 
 //-----------------------------------------------------//
 
 
 //EJ14
 
+/*
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Introduzca a continuacion su nombre: 
+    <input name="nombrePersona" size="25" id="nombrePersona" >
+    <br>
+    <button onclick="listarPersonas()">Registrar</button>
+    <ul id="listaDesordenada">
+        <li></li>
+    </ul>
+    <script>
+        let listaPersonas =['hola'];
+        let contador = 0;
+        function concatenarValores(array,valor,posicion){
+            array[posicion] = valor;
+        }
+
+        function agregarListaAWeb(lista){
+            let li = "";
+            for (let i = 0; i < lista.lenght ; i ++){
+                li = "<li>" + lista[i] + "<li>";
+            }
+            document.getElementById("listaDesordenada").innerHTML = lista;
+        }
+
+        function listarPersonas(){
+            let persona = document.getElementById("nombrePersona").value;
+            concatenarValores(listaPersonas,persona,contador);
+            agregarListaAWeb(listaPersonas);
+            console.log(listaPersonas);
+            contador ++;
+        }
+
+        
+    </script>
+</body>
+</html>
+*/
 
 //-----------------------------------------------------//
---
+++ Clase 5/GUIA DE EJERCICIO 5/prueba.js
@@ -0,0 +1,12 @@
+function lista(lista){
+    let frutas = ["banana","naranja"];
+    let auxiliar = lista.length;
+    console.log(auxiliar);
+    //console.log(lista);
+    for(i = 0; i < auxiliar.length; i++){
+            console.log(frutas[i]);
+    }
+}
+
+let vector = ["kaka","messi","ronaldo"];
+lista(vector);
